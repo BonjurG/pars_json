2a173569bfc64342d7ecda1e2fc3df39f98eaa7b
BonjurG <hmgrem@yandex.ru>
2022-07-15 13:56:41 +0500

    some fix

--- pars_json1.py
+++ pars_json1.py
@@ -1,6 +1,6 @@
 import json
 
-max = 0
+maxi = 0
 max_name = ''
 max_lastname = ''
 with open('manager_sales.json', 'r') as file:
@@ -14,8 +14,8 @@ with open('manager_sales.json', 'r') as file:
         for car in element['cars']:
             total += car['price']
 
-        if total > max:
-            max = total
+        if total > maxi:
+            maxi = total
             max_name = name
             max_lastname = lastname
-    print(max_name, max_lastname, max)
+    print(max_name, max_lastname, maxi)
